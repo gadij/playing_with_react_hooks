160f62dcdd181904c65085809a0c9a530ac1bcb5
gadij00@gmail.com <gadij00@gmail.com>
2020-01-01 16:15:56 +0200

    basic style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// import "./styles.css";
+import "./styles.scss";
 // import ClassCounter from "./components/Counters/ClassCounter";
 // import HookCounter from "./components/Counters/HookCounter";
 // import ClassMouse from "./components/MouseHover/ClassMouse";
